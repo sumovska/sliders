aec0e86ba3767bc7dc3021e21cb711c2443c189c
Nataliia <sumovskaya@gmail.com>
2016-08-11 17:01:12 +0700

    initial build

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+/*global window, document, $, google, mapCenter, FastClick */
+
+/** Remove tap delay on touch devices */
+FastClick.attach(document.body);
+
+/** On document ready */
+$(document).ready(function() {
+
+
+});
